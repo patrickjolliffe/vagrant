71410886c16b10ca13139e50a45719672da4a058
Patrick Jolliffe <jolliffe@gmail.com>
2019-07-25 14:54:03 +0800

    Rework test suite

--- ords-reverse-proxy/scripts/general.sh
+++ ords-reverse-proxy/scripts/general.sh
@@ -12,14 +12,16 @@ EOF
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y epel-release-latest-7.noarch.rpm
 rm epel-release-latest-7.noarch.rpm
-
 yum install -y htop jq mlocate strace
 
-mkdir /home/vagrant/bin
-cat > /home/vagrant/bin/orpdemo << EOF
-#!/usr/bin/bash
-/vagrant/scripts/orpdemo.sh \$1
-EOF
+sed -i 's/PATH=/ i ' /home/vagrant/.bash_profile
+sed -i '/PATH=/ s/$/:$HOME\/testsuite/' /home/vagrant/.bash_profile
 
-chown vagrant:vagrant /home/vagrant/bin/orpdemo
-chmod u+x /home/vagrant/bin/orpdemo
+cat >> .bash_profile << EOF
+printf '\e[?1000l'         #Try to fix mouse getting screwy in iterm2
+echo "Welcome to the ORDS Reverse Proxy Vagrant Machine"
+echo "Please type orp-all to run the entire test suite"
+echo "An optional duration parameter can be provided, using same format as siege"
+echo "For example 10S (seconds) or 2M (minute)."
+echo "If this is not provided the default is 1 Minute"
+EOF
