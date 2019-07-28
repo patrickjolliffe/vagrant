ab57103a5df366e7469210c9b8c16da03341247c
Patrick Jolliffe <jolliffe@gmail.com>
2019-07-28 13:46:02 +0800

    More tidying up

--- ords-reverse-proxy/scripts/general.sh
+++ ords-reverse-proxy/scripts/general.sh
@@ -20,9 +20,5 @@ sed -i '/PATH=/ s/$/:$HOME\/testsuite/' /home/vagrant/.bash_profile
 cat >> .bash_profile << EOF
 printf '\e[?1000l'         #Try to fix mouse getting screwy in iterm2
 echo "Welcome to the ORDS Reverse Proxy Vagrant Machine"
-echo "Please type orp-all to run the entire test suite"
-echo "An optional duration parameter can be provided, using same format as siege"
-echo "For example 10S (seconds) or 2M (minute)."
-echo "If this is not provided the default is 1 Minute"
-echo "To just compare direct vs reverse proxy use orp-combos"
+echo "orp-demo will run the entire test suite"
 EOF
